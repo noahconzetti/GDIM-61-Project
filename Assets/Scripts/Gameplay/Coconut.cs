ad1830cf4a20887314966985f04704f24e3cc578
noahconzetti <nconzetti@gmail.com>
2026-04-03 13:34:13 -0700

    Jumping improvements

--- Assets/Scripts/Gameplay/Coconut.cs
+++ Assets/Scripts/Gameplay/Coconut.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using AppCore;
 using UnityEngine;
 
@@ -7,18 +9,26 @@ namespace Gameplay {
         [SerializeField] private float jumpForce = 2;
         [Tooltip("This is normalized when jump is called")]
         [SerializeField] private Vector2 jumpDirection;
+        [SerializeField] private float jumpBufferTime = .3f;
+        [Header("Grounded Settings")]
+        [SerializeField] private LayerMask groundLayer;
+        // [SerializeField] private Vector2 groundCastRelativeOrigin;
+        [SerializeField] private float groundCastDistance = .2f;
 
         private Vector2 _jumpDirectionNormalized;
 
         private Rigidbody2D _rb;
 
+        private bool _grounded = false;
+        private bool _jumpBufferActive = false;
+
         private void OnEnable() {
-            InputManager.OnPlayerJump += HandleJump;
+            InputManager.OnPlayerJump += HandleJumpInput;
             InputManager.OnPlayerAbility += HandleAbility;
         }
     
         private void OnDisable() {
-            InputManager.OnPlayerJump -= HandleJump;
+            InputManager.OnPlayerJump -= HandleJumpInput;
             InputManager.OnPlayerAbility -= HandleAbility;
         }
 
@@ -27,14 +37,42 @@ namespace Gameplay {
             _jumpDirectionNormalized = jumpDirection.normalized;
         }
 
+        private void Update() {
+            _grounded = Physics2D.Raycast(
+                _rb.position,
+                Vector2.down,
+                groundCastDistance,
+                groundLayer.value);
+            if (_grounded && _jumpBufferActive) {
+                Jump();
+            }
+        }
+
         private void HandleAbility(int id) {
             if (id != playerID) return;
             Debug.Log("Using ability!");
         }
 
-        private void HandleJump(int id) {
+        private void HandleJumpInput(int id) {
             if (id != playerID) return;
-            _rb.AddForce(_jumpDirectionNormalized * jumpForce, ForceMode2D.Impulse);
+            StopAllCoroutines();
+            if (!_grounded) {
+                StartCoroutine(SaveJumpInput());
+                return;
+            }
+
+            Jump();
+        }
+
+        private void Jump() {
+            _rb.linearVelocity = _jumpDirectionNormalized * jumpForce;
+            _jumpBufferActive = false;
+        }
+
+        private IEnumerator SaveJumpInput() {
+            _jumpBufferActive = true;
+            yield return new WaitForSeconds(jumpBufferTime);
+            _jumpBufferActive = false;
         }
     }
 }
