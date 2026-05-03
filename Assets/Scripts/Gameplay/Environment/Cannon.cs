29728683668fc322782a8b17c596384f56e6ed4a
noahconzetti <nconzetti@gmail.com>
2026-05-02 22:03:23 -0700

    Added cannon updates

--- Assets/Scripts/Gameplay/Environment/Cannon.cs
+++ Assets/Scripts/Gameplay/Environment/Cannon.cs
@@ -6,8 +6,29 @@ namespace Gameplay.Environment {
     public class Cannon : MonoBehaviour {
         [SerializeField] private float waitTime = 0.5f;
         [SerializeField] private float shootForce = 10f;
+
+        [SerializeField] private float rotationAmount = 20f;
+        [SerializeField] private float rotationSpeed = 5f;
         private Coconut _coconut = null;
-        
+
+        private float _startRotation;
+
+        private void Start() {
+            _startRotation = transform.rotation.eulerAngles.z;
+        }
+
+        private void Update() {
+            if (!_coconut) {
+                RotateCannon();
+            }
+        }
+
+        private void RotateCannon() {
+            float newRotation = _startRotation + Mathf.Sin(Time.time * rotationSpeed) * rotationAmount;
+            transform.rotation = Quaternion.Euler(0, 0, newRotation);
+            Debug.Log(newRotation);
+        }
+
         private void OnTriggerEnter2D(Collider2D other) {
             if (_coconut != null) return;
             if (other.TryGetComponent(out Coconut coconut)) {
